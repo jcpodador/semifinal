3e48f9b6fa6baab6cf34a8d11d2282a45f359cd8
jcpodador <71476611+jcpodador@users.noreply.github.com>
2020-11-30 16:55:24 +0800

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,7 @@
 @extends('base')
 
 @section('content')
+
 @if($info = Session::get('info'))
 
     <div class="card">
@@ -10,31 +11,32 @@
     </div>
 
 @endif
-    <div class="float-right">
-       <a href="{{url('/instructors/create')}}" class="btn btn-primary">
-       Add New Instructor
-       </a>
-    </div>
 
-<h1>Instructors</h1>
-
+<div class="float-right">
+    <a href="{{url('/users/create')}}" class="btn btn-primary">
+        Add New User
+    </a>
+</div>
+<h1>List of Users</h1>
 <table class="table table-bordered table-striped table-sm">
     <thead>
-        <th>ID#</th>
-        <th>Last Name</th>
-        <th>First Name</th>
-        <th>Expertise</th>
-        <th></th>
+        <tr>
+            <th>ID#</th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Email</th>
+            <th></th>
+        </tr>
     </thead>
     <tbody>
-        @foreach($instructors as $i)
+        @foreach($users as $u)
 
         <tr>
-            <td>{{$i->id}}</td>
-            <td>{{$i->user->lname}}</td>
-            <td>{{$i->user->fname}}</td>
-            <td>{{$i->aoe}}</td>
-            <td><a href="{{url('/instructors/edit', ['id'=>$i])}}" class="btn btn-secondary btn-sm">...</a></td>
+            <td>{{$u->id}}</td>
+            <td>{{$u->lname}}</td>
+            <td>{{$u->fname}}</td>
+            <td>{{$u->email}}</td>
+            <td><a href="{{url('/users/edit', ['id'=>$u])}}" class="btn btn-secondary btn-sm">...</a></td>
         </tr>
 
         @endforeach
